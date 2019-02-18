6b8bb6a482f01487fe588816eb05cc7b2c7ccb7e
Aining Wang <aining.wang@tigergraph.com>
2019-02-18 14:20:16 -0800

    turn txt into gsql

--- Golang/blockchain_parse_csv.go
+++ Golang/blockchain_parse_csv.go
@@ -53,15 +53,16 @@ func main() {
 	flag.Parse()
 
 	if printHelp {
-		fmt.Println("-----------------------------")
 		flag.PrintDefaults()
-		fmt.Println("-----------------------------")
 		os.Exit(0)
 	}
 
 	// check data directory exist or not
 	if _, err := os.Stat(database); os.IsNotExist(err) {
 		fmt.Println("Database directory doesn't exist!")
+		fmt.Println("-----------------------------------------")
+		flag.PrintDefaults()
+		fmt.Println("-----------------------------------------")
 		os.Exit(1)
 	}
 
@@ -71,6 +72,9 @@ func main() {
 		errMkdir := os.MkdirAll(outputDir, os.ModePerm)
 		if errMkdir != nil {
 			fmt.Println("Cannot create", outputDir)
+			fmt.Println("-----------------------------------------")
+			flag.PrintDefaults()
+			fmt.Println("-----------------------------------------")
 			os.Exit(1)
 		} 
 		fmt.Println(outputDir," created")
